10d9ba695315e697041e8cdfb088cd9968a42397
Jason Brown <browniefed@gmail.com>
2025-04-06 18:49:23 -0700

    Fixup

--- index.mjs
+++ index.mjs
@@ -1,10 +1,11 @@
 import dependencyTree from "./dependency-tree.js";
 import fs from "fs";
+import { config } from "./configuration.js";
 
 const tree = dependencyTree({
-  filename: "", // entry point (specific route)
-  directory: "", // root directory
-  tsConfig: "", // root tsconfig file with tsconfig paths
+  filename: config.filename,
+  directory: config.directory,
+  tsConfig: config.tsConfig,
   nodeModulesConfig: {
     entry: "module",
   },
