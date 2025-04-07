0036bd932be3bbb46ccfc882f2ef0f0983e78403
Jason Brown <browniefed@gmail.com>
2025-04-06 18:45:46 -0700

    Undo

--- index.mjs
+++ index.mjs
@@ -2,10 +2,9 @@ import dependencyTree from "./dependency-tree.js";
 import fs from "fs";
 
 const tree = dependencyTree({
-  filename:
-    "/Users/jasonbrown/companies/digs/web/app/(protected)/projects/(index)/page.tsx", // entry point (specific route)
-  directory: "/Users/jasonbrown/companies/digs", // root directory
-  tsConfig: "/Users/jasonbrown/companies/digs/web/tsconfig.json", // root tsconfig file with tsconfig paths
+  filename: "", // entry point (specific route)
+  directory: "", // root directory
+  tsConfig: "", // root tsconfig file with tsconfig paths
   nodeModulesConfig: {
     entry: "module",
   },
